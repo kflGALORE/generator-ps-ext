459318de5c7553495b9aa9ae5258ec1abeecee56
kflGALORE <kflgalore@gmail.com>
2018-04-15 17:32:22 +0200

    #0 Added initial sources

---
+++ generators/app/index.js
@@ -0,0 +1,95 @@
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+	constructor(args, opts) {
+		super(args, opts);
+		
+		this.context = {
+			extension: {
+				photoshop: {}
+			},
+			
+			applyAnswers(answers) {
+				this.extension.id = answers.extensionId;
+				this.extension.name = answers.extensionName;
+				this.extension.photoshop.versionName = answers.psVersionName[0];
+			}
+		}
+	}
+	
+	prompting() {
+		return this.prompt([
+				{
+					type: 'input',
+					name: 'extensionId',
+					message: 'Extension ID:',
+					validate: Validations.valueIsId
+				},
+				{
+					type: 'input',
+					name: 'extensionName',
+					message: 'Extension name:',
+					validate: Validations.valueIsSpecified
+				},
+				{
+					type: 'checkbox',
+					name: 'psVersionName',
+					choices: ['CC 2014', 'CC 2015'],
+					default: ['CC 2015'],
+					message: 'Photoshop version:',
+					validate: Validations.exactlyOneOptionIsSelected
+				}
+			])
+			.then((answers) => {
+				this.context.applyAnswers(answers);
+			});
+	};
+	
+	debug() {
+		console.log('context: ' + JSON.stringify(this.context));
+	};
+	
+	writing() {
+		console.log('writing ...');
+		console.log('sourceRoot=' + this.sourceRoot());
+		console.log('destinationPath=' + this.destinationPath('tmp'));
+		console.log('templateGlobPath=' + this.templatePath('**'));
+		this.fs.copyTpl(this.templatePath('**'), this.destinationPath(this.context.extension.id), this.context);
+	};
+
+};
+
+class Validations {
+	
+	static valueIsSpecified(input) {
+		if (! input || input.trim().length === 0) {
+			return 'You must specify a value';
+		} else {
+			return true;
+		}
+	}
+	
+	static valueIsId(input) {
+		var valueIsSpecified = Validations.valueIsSpecified(input);
+		if (valueIsSpecified === true) {
+			var regex = '[A-Za-z0-9]+[A-Za-z0-9_\\-\\.]?[A-Za-z0-9]?';
+			if (! input.match(regex)) {
+				return 'Specified value is not a valid ID.\nA valid ID must match the regular expression: ' + regex;
+			} else {
+				return true;
+			}
+		} else {
+			return valueIsSpecified;
+		}
+	}
+	
+	static exactlyOneOptionIsSelected(input) {
+		if (input.length < 1) {
+			return "You must select an option";
+		} else if (input.length > 1) {
+			return "You may select one option only";
+		} else {
+			return true;
+		}
+	}
+}
