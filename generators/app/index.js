0c147f6a7a9c63be4a573b89cf04e3d82a4a3a64
kflGALORE <kflgalore@gmail.com>
2018-05-05 18:42:39 +0200

    #0 Added ui templates and installation of npm packages with CEP download

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,4 +1,8 @@
-var Generator = require('yeoman-generator');
+const Generator = require('yeoman-generator');
+
+const fs = require('fs');
+const makedir = require('make-dir');
+const http = require('request');
 
 module.exports = class extends Generator {
 	constructor(args, opts) {
@@ -6,7 +10,8 @@ module.exports = class extends Generator {
 		
 		this.context = {
 			extension: {
-				photoshop: {}
+				photoshop: {},
+				cep: {}
 			},
 			
 			applyAnswers(answers) {
@@ -34,22 +39,97 @@ module.exports = class extends Generator {
 				{
 					type: 'checkbox',
 					name: 'psVersionName',
-					choices: ['CC 2014', 'CC 2015'],
+					choices: ['CC 2014', 'CC 2015', 'CC 2015.1', 'CC 2015.5', 'CC 2017', 'CC 2018'],
 					default: ['CC 2015'],
 					message: 'Photoshop version:',
 					validate: Validations.exactlyOneOptionIsSelected
-				}
+				}	
 			])
 			.then((answers) => {
 				this.context.applyAnswers(answers);
 			});
 	};
 	
+	configuring() {
+		this.extensionPath = this.destinationPath(this.context.extension.id);
+		
+		const psMapping = psMappings[this.context.extension.photoshop.versionName];
+		this.context.extension.photoshop.versionId = psMapping.photoshop.versionId;
+		this.context.extension.cep.versionId = psMapping.cep.versionId;
+		this.context.extension.cep.downloadURL = psMapping.cep.downloadURL;
+	};
+	
 	writing() {
-		this.fs.copyTpl(this.templatePath('**'), this.destinationPath(this.context.extension.id), this.context);
+		this.fs.copyTpl(this.templatePath('**'), this.extensionPath, this.context);
+	};
+	
+	install() {
+		this._npmInstall();
+		this._npmInstall('host');
+		this._npmInstall('ui');
+		this._downloadCEP('ui');
+	};
+	
+	_npmInstall(module) {
+		var dir = this.extensionPath;
+		if (module) {
+			dir = dir + '/' + module;
+		}
+		
+		this.spawnCommandSync('npm', ['install'], {"cwd": dir});
+	};
+	
+	_downloadCEP(module) {
+		var cepFileNames = ['CSInterface.js', 'Vulcan.js', 'AgoraLib.js'];
+		var cepBaseURL = this.context.extension.cep.downloadURL;
+		var targetDir = this.extensionPath + '/' + module + '/libs/cep';
+		
+		if (! fs.existsSync(targetDir)) {
+			makedir.sync(targetDir);
+		}
+		
+		for (let i = 0; i < cepFileNames.length; i++) {
+			let cepFileName = cepFileNames[i];
+			let cepFileURL = cepBaseURL + '/' + cepFileName;
+			let targetFile = targetDir + '/' + cepFileName;
+			
+			this.log('download ' + cepFileURL);
+			http.get(cepFileURL, {timeout: 10000})
+				.on('error', error => {
+					throw new Error(error);
+				})
+				.pipe(fs.createWriteStream(targetFile));
+		}
 	};
 
 };
+
+const psMappings = {
+	"CC 2014": {
+        photoshop: {versionId: "15.0"},
+        cep: {versionId: "5.0", downloadURL: "http://raw.githubusercontent.com/Adobe-CEP/CEP-Resources/master/CEP_5.x"}
+    },
+    "CC 2015": {
+		photoshop: {versionId: "16.0"},
+        cep: {versionId: "6.0", downloadURL: "http://raw.githubusercontent.com/Adobe-CEP/CEP-Resources/master/CEP_6.x"}
+    },
+    "CC 2015.1": {
+		photoshop: {versionId: "17.0.2"},
+        cep: {versionId: "7.0", downloadURL: "http://raw.githubusercontent.com/Adobe-CEP/CEP-Resources/master/CEP_7.x"}
+    },
+    "CC 2015.5": {
+		photoshop: {versionId: "17.0.2"},
+        cep: {versionId: "7.0", downloadURL: "http://raw.githubusercontent.com/Adobe-CEP/CEP-Resources/master/CEP_7.x"}
+    },
+    "CC 2017": {
+		photoshop: {versionId: "18.0"},
+        cep: {versionId: "7.0", downloadURL: "http://raw.githubusercontent.com/Adobe-CEP/CEP-Resources/master/CEP_7.x"}
+    },
+    "CC 2018": {
+		photoshop: {versionId: "19.0"},
+        cep: {versionId: "8.0", downloadURL: "http://raw.githubusercontent.com/Adobe-CEP/CEP-Resources/master/CEP_8.x"}
+    }
+}
 
 class Validations {
 	
